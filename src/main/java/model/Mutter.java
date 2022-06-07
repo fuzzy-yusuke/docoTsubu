fd3546c829c6e6edd94075e54f97bc7b81a56719
fuzzy-yusuke <youth_k0228@outlook.com>
2022-06-07 18:17:14 +0900

    修正

--- src/main/java/model/Mutter.java
+++ src/main/java/model/Mutter.java
@@ -8,6 +8,13 @@ public class Mutter implements Serializable{
 	private String userName;	//ユーザー名
 	private String text;		//つぶやき内容
 	public Mutter() {}
+	//つぶやきに関する情報を保持
+	public Mutter(String userName,String text) {
+		this.userName=userName;
+		this.text=text;
+	}
+	
+	//DBと連動
 	public Mutter(int id,String userName,String text) {
 		this.id=id;
 		this.userName=userName;
